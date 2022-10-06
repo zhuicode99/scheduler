8ba89bf0e1ddab5295d3ff895340031f6c4fb33a
Enlin(Jason) Chen <enlin291814833@hotmail.com>
2022-10-05 20:28:14 -0400

    added daylist component to application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,30 @@
 import React from "react";
 
 import "components/Application.scss";
+import DayList from "components/DayList";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
 
 export default function Application(props) {
+
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,6 +34,11 @@ export default function Application(props) {
   alt="Interview Scheduler"
 />
 <hr className="sidebar__separator sidebar--centered" />
+<DayList
+  days={days}
+  day={"Monday"}
+  setDay={day => console.log(day)}
+/>
 <nav className="sidebar__menu"></nav>
 <img
   className="sidebar__lhl sidebar--centered"
