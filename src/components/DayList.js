e97449f086236b6eae6eb37d7ad59a5585d842f1
Enlin(Jason) Chen <enlin291814833@hotmail.com>
2022-10-06 00:03:33 -0400

    update day and setday to value and onChange

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,12 +6,12 @@ export default function DayList(props) {
   const dayListItems = props.days.map((day) => {
     return (
       <DayListItem
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
-      />
+      key={day.id} 
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.value}
+      setDay={props.onChange}
+    />
     );
   });
 
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,13 +5,13 @@ import InterviewerListItem from "./InterviewerListItem";
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map((interviewer) => {
     return (
-      <InterviewerListItem
-        key={interviewer.id}
-        name={interviewer.name}
-        avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
-      />
+      <InterviewerListItem 
+         key={interviewer.id}
+         name={interviewer.name}
+         avatar={interviewer.avatar}
+         selected={interviewer.id === props.value}
+         setInterviewer={() => props.onChange(interviewer.id)}    
+/>
     );
   });
 
