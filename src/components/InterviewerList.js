0e54d0daefe8e7a53b4ce07f08ea24957f2e09c9
Enlin(Jason) Chen <enlin291814833@hotmail.com>
2022-10-05 23:40:00 -0400

    refactor interviewerlistitem and interviewerlist components with story

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,22 +3,22 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-  const interviewerListItem = props.interviewers.map((interviewer) => {
+  const interviewers = props.interviewers.map((interviewer) => {
     return (
-			<InterviewerListItem
-				key={interviewer.id}
-				name={interviewer.name}
-				avatar={interviewer.avatar}
-				selected={interviewer.id === props.value}
-				setInterviewer={props.setInterviewer}
-			/>
-		);
-	});
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+      />
+    );
+  });
 
     return (
       <section className="interviewers">
         <h4 className="interviewers__header text--light">Interviewer</h4>
-        <ul className="interviewers__list">{interviewerListItem}</ul>
+        <ul className="interviewers__list">{interviewers}</ul>
       </section>
     );
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -18,6 +18,7 @@ export default function InterviewerListItem(props) {
       alt={props.name}
     />
     {props.selected ? props.name : null}
+    {/* {props.selected && props.name} */}
   </li>
 
    );
