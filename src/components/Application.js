ba062684c51bf61f960a4173a688463c7a5c9b6e
Enlin(Jason) Chen <enlin291814833@hotmail.com>
2022-10-10 10:09:37 -0400

    import helper function to help extract appointments data for different days

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
-
+import { getAppointmentsForDay } from "helpers/selectors";
 
 
 /* const appointments = {
@@ -56,15 +56,20 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state,state.day);
 
   const setDay = day => setState({ ...state, day });
-  const setDays = (days) => setState(prev => ({ ...prev, days }));
   
   
-  useEffect(() => {
-    axios.get("/api/days").then(response => setDays(response.data));
-  }, []);
+  useEffect(()=>{
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ]).then((all)=>{
+      setState(prev => ({...prev, days: all[0].data, appointments:all[1].data, interviewers:  all[2].data }));
+    })
+  },[])
   
 
 
