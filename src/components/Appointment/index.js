f44d0e2bf2257399376e26a448e2724d034c88fb
Enlin(Jason) Chen <enlin291814833@hotmail.com>
2022-10-10 22:19:06 -0400

    add custom hook for empty and show components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,37 @@ import 'components/Appointment/styles.scss';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
+import useVisualMode from "../../hooks/useVisualMode"
+import Form from 'components/Appointment/Form';
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE ="CREATE";
 
+export default function Appointment(props) { 
+  
+  const {mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
-export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header
       time={props.time}
       />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>}
+      {mode === SHOW && 
+        <Show 
+          student={props.interview.student} 
+          interviewer={props.interview.interviewer} 
+          onEdit={null} 
+          onDelete={null} />}
+      {mode === CREATE &&   
+        <Form 
+          interviewers={[]} 
+          onSave={null} 
+          onCancel={() => back()}/>}
     </article>
   );
 };
+
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition (newMode, replace = false) {
+    if (replace) { 
+      setMode(newMode);
+    } else {
+      setMode(newMode) 
+      setHistory(prev => [...prev, newMode])
+    }
+  }
+
+  function back() {
+    history.pop()
+
+    if (history.length > 0) {
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
